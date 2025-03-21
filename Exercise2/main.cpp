f62ee3e90b939abfeff33c633b3dc0912222c7f0
cael kyler <s307931@studenti.polito.it>
2025-03-21 17:41:43 +0100

    Esercizio svolto e aggiornato

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -12,13 +12,25 @@ anche per aiutarmi ad ottimizzare il codice in una forma più corto ed efficient
 #include <string>
 #include <vector>
 
+/*
+Per mappare un punto x dell'intervallo [a,b]=[1,5] ad un punto y dell'intervallo [c,d]=[-1,2]
+usiamo la formula seguente: y= c+ (((x-a)/(b-a)))*(d-c)= -1 + ((x-1)/4)*3 .
 
-int main()
-{
-    // per comodità mi salvo nel ifileName il nome del file che mi serve
+Quindi scrivo una funzione che svolge tale mappatura.
+*/
 
-    std::string ifileName = "data.txt";
-    std::ifstream infile(ifileName);    // apro il file in modalità lettura
+double map(double x) {
+    double mappedVal = -1 + (x-1) * 3/4 ;
+    return mappedVal;
+}
+
+// MAIN
+
+int main() {
+    // per comodità mi salvo nel infileName il nome del file che mi serve
+
+    std::string infileName = "data.txt";
+    std::ifstream infile(infileName);    // apro il file in modalità lettura
 
 
     // è bene controllare che il file si apra correttamente; se non si apre, allora segnalo l'errore
@@ -30,28 +42,16 @@ int main()
 
     //Dato che i risultati devono essere stampati su un file result.txt, creo il file e ci scrivo già gli header.
 
-    std::string  ofileName = "result.txt";
-    std::ofstream outfile(ofileName);   //apro il file in writing mode
+    std::string  outfileName = "result.txt";
+    std::ofstream outfile(outfileName);   //apro il file in writing mode
 
     if (!outfile.is_open()) {
         std:: cerr << "Il file non si apre correttamente." << std::endl;   // un messaggio che indica che il file non si apre bene
         return 1;   // mi segnala che il file non si apre bene
     }
 
-    ofile << "# N Mean" << std::endl;   //header
+    outfile << "# N Mean" << std::endl;   //header
 
-
-    /*
-    Per mappare un punto x dell'intervallo [a,b]=[1,5] ad un punto y dell'intervallo [c,d]=[-1,2]
-    usiamo la formula seguente: y= c+ (((x-a)/(b-a)))*(d-c)= -1 + ((x-1)/4)*3 .
-
-    Quindi scrivo una funzione che svolge tale mappatura.
-    */
-
-    double map(double x) {
-        double mappedVal = -1 + (x-1) * 3/4 ;
-        return mappedVal;
-    }
 
     /*
     Ora leggo il valore di ogni riga in formato double;
@@ -59,20 +59,17 @@ int main()
     Lo raccolgo in un vettore.
     */
 
-    std::vector<double> valList;
+    std::vector<double> valList;   //valList è un vector o lista di valori double
     std::string line;
-    double val;
 
     while (getline(infile, line)) {
 
-        val = std::stod(line);   //trasforma il valore di ogni riga in double
-
+        double val = std::stod(line);   //trasforma il valore di ogni riga in double
         double valFin = map(val);   //uso la funzione map per mappare un valore val nel nuovo intervallo
-
         valList.push_back(valFin);   //aggiunge nel vector valList il valore mappato valFin 
     }
 
-    inFile.close()   //La inFile non mi serve più, per cui la chiudo.
+    infile.close();   //La inFile non mi serve più, per cui la chiudo.
 
     /*
     Ora che ho la vector di valori mappati, posso calcolare la media secondo le istruzioni;
@@ -82,14 +79,13 @@ int main()
     double sum = 0.0;   // è la somma dei primi i valori della lista per ogni i
 
     for (size_t i = 0; i <valList.size(); ++i) {
-
-        sum += sum[i];
+        sum += valList[i];
         double mean = sum/ (i+1);
 
-        outFile << i+1 << " " << mean;
+        outfile << i+1 << " " << std::scientific << mean << std::endl;
     }
 
-    outFile.close()   //ricordo di chiudere l'outFile
+    outfile.close();   //ricordo di chiudere l'outFile
 
     return 0;
 }
